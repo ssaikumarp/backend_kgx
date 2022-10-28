7c5476763734bfdab4b4df6282ab3a33c78c2ac8
SASI KUMAR P <59645728+ssaikumarp@users.noreply.github.com>
2022-10-28 20:12:09 +0530

    Add files via upload

---
+++ REST-API/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+department = [{'dept_id' : '01', 'dept_name' : 'CSE','description':'programmer'},
+{'dept_id' : '02', 'dept_name' : 'IT','description':'software engineering'},
+{'dept_id' : '03', 'dept_name' : 'ECE','description':'electronics'}]
+
+@app.route('/')
+def index():
+    return "welcome to the course API"
+
+@app.route("/department",methods=['GET'])
+def get():
+    return jsonify({'department':department})
+
+
+@app.route("/department/<int:dept_id>",methods=['GET'])
+def get_department(dept_id):
+    return jsonify({'department':department[dept_id]})
+
+
+@app.route("/department",methods=['POST'])
+def create():
+    dept={'dept_id' : '04', 'dept_name' : 'CSBS','description':'Computer programmer'}
+    department.append(dept)
+    return jsonify({'create':dept})
+
+@app.route("/department/<int:dept_id>",methods=['DELETE'])
+def delete(dept_id):
+    department.remove(department[dept_id])              
+    return jsonify({'result':True})
+
+
+if __name__ =="__main__":
+    app.run(debug=True)    
